a9c5100e5a4b14c7f8534974ccb553d1b07d4584
kminch215 <kendraminch1036@gmail.com>
2024-05-01 11:30:52 -0500

    Updated main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,18 @@
+import javax.swing.SwingUtilities;
 
+import controller.FlightListController;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				FlightListController flightController = new FlightListController();
+				flightController.initiate();
+			}
+		});
+		
 	}
 
 }

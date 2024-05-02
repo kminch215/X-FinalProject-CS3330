71af8097db493c964377711875b5eaefd578adfa
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-02 13:44:28 -0500

    Added print methods for UserID, Current Tickets, and Ticket History

--- src/main/java/view/UserDashboardView.java
+++ src/main/java/view/UserDashboardView.java
@@ -1,5 +1,15 @@
 package view;
 
 public class UserDashboardView {
-
+    public void displayUserProfile(int userID) {
+        System.out.println("User Profile for UserID: " + userID);
+    }
+    
+    public void displayCurrentTickets() {
+        System.out.println("Displaying Current Tickets...");
+    }
+    
+    public void displayTicketHistory() {
+        System.out.println("Displaying Ticket History...");
+    }
 }

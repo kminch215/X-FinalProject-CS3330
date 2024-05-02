877abcf1a5080907c4979b64b1ea40abd4cf5c86
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-02 14:39:07 -0500

    Update UserDashboardController.java

--- src/main/java/controller/UserDashboardController.java
+++ src/main/java/controller/UserDashboardController.java
@@ -1,16 +1,60 @@
 package controller;
 
-import model.UserDashboard;
-import view.UserDashboardView;
+import model.UserListModel;
+import view.UserLoginView;
 
-public class UserDashboardController {
-
-	private UserDashboardView dashboardView;
-	private UserDashboard dashboardModel;
+public class UserLoginController {
 	
-	public UserDashboardController() {
+	private UserLoginView loginView;
+	private UserListModel loginModel;
+	private UserInformation currentUser;
+	private static UserLoginController instance;
+	
+	
+	public UserLoginController() {
 		super();
-		this.dashboardView = new UserDashboardView();
-		this.dashboardModel = new UserDashboard(null); //change this to input the userID
+		this.loginView = new UserLoginView();
+		this.loginModel = new UserListModel();
 	}
+
+	public static UserLoginController getInstance() {
+        if (instance == null) {
+            instance = new UserLoginController();
+        }
+        return instance;
+    }
+    
+    public void userLogin(String username, String password) {
+        if (validateInput(username, password)) {
+            UserInformation user = loginModel.authenticateUser(username, password);
+            if (user != null) {
+                // Login successful
+                currentUser = user;
+                loginView.displaySuccessMessage("Login successful.");
+            } else {
+                // Login failed
+                loginView.displayErrorMessage("Invalid username or password.");
+            }
+        } else {
+            loginView.displayErrorMessage("Invalid input. Please provide username and password.");
+        }
+    }
+    
+    public void logout() {
+        currentUser = null;
+        // Redirect to login page
+    }
+    
+    private boolean validateInput(String username, String password) {
+        return !username.isEmpty() && !password.isEmpty();
+    }
+    
+    public boolean isLoggedIn() {
+        return currentUser != null;
+    }
+    
+    public UserInformation getCurrentUser() {
+        return currentUser;
+    }
+	
 }

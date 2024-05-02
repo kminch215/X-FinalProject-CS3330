cd200384f74993146d437bbac5cd97ec023903e5
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-02 14:35:33 -0500

    Revert UserDashboardController.java

--- src/main/java/controller/UserDashboardController.java
+++ src/main/java/controller/UserDashboardController.java
@@ -1,53 +1,5 @@
-package controller;
-
-import model.UserDashboard;
-import view.UserDashboardView;
-
-public class UserDashboardController {
-
-	private UserDashboardView dashboardView;
-	private UserDashboard dashboardModel;
-	
-	public UserDashboardController() {
+public UserDashboardController() {
 		super();
 		this.dashboardView = new UserDashboardView();
-		this.dashboardModel = new UserDashboard(user.getUserID());
-	}
-
-	public void displayDashboard() {
-        loadUserData();
-        
-        // Display user dashboard
-        dashboardView.displayUserProfile(dashboardModel.getUserID());
-        dashboardView.displayCurrentTickets();
-        dashboardView.displayTicketHistory();
-    }
-    
-    private void loadUserData() {
-        ArrayList<ReceiptInformation> purchaseHistory = new ArrayList<>();
-	dashboardModel.setPurchaseHistory(purchaseHistory);
-    }
-    
-    public void updateProfile(String firstName, String lastName, String email) {
-        // Update user profile information
-        dashboardModel.getCurrentUser().setFirstName(firstName);
-        dashboardModel.getCurrentUser().setLastName(lastName);
-        dashboardModel.getCurrentUser().setEmail(email);
-    }
-    
-    public void viewTicketDetails(int ticketID) {
-        System.out.println("Ticket Details:");
-        System.out.println("Ticket ID: " + ticketID);
-        System.out.println("Flight Name: " + dashboardModel.getFlightName(ticketID));
-        System.out.println("Date: " + dashboardModel.getFlightDate(ticketID));
-        System.out.println("Price: " + dashboardModel.getFlightPrice(ticketID));
-    }
-    
-    public void cancelTicket(int ticketID) {
-        dashboardModel.removeTicket(ticketID);
-	System.out.println("Ticket with ID " + ticketID + " cancelled successfully.");
-	displayDashboard();
-    }
-	
+		this.dashboardModel = new UserDashboard(null); //change this to input the userID
 }
-

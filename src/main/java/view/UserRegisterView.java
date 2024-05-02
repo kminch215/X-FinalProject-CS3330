c150fe0151d1dc868df8b6b7b6cc62f1d974928a
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-02 13:41:20 -0500

    Added scanners, set up UserRegisterView() and displayRegisterForm()

--- src/main/java/view/UserRegisterView.java
+++ src/main/java/view/UserRegisterView.java
@@ -1,5 +1,28 @@
 package view;
 
-public class UserRegisterView {
+import java.util.Scanner;
 
+public class UserRegisterView {
+    private Scanner scanner;
+    
+    public UserRegisterView() {
+        scanner = new Scanner(System.in);
+    }
+    
+    public void displayRegisterForm() {
+        System.out.println("=== User Registration ===");
+        System.out.print("Enter username: ");
+        String username = scanner.nextLine();
+        System.out.print("Enter password: ");
+        String password = scanner.nextLine();
+        System.out.print("Enter first name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter email: ");
+        String email = scanner.nextLine();
+        
+        // Pass the user input to the controller for registration
+        UserRegisterController.getInstance().registerUser(username, password, firstName, lastName, email);
+    }
 }

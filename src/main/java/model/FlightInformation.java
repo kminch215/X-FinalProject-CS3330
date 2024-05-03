e40ab3c08f602f9851d30a05fb96845c7a0e299c
kminch215 <kendraminch1036@gmail.com>
2024-05-03 14:53:57 -0500

    Removing the setters
    The setters for this model are not needed because we do not want any
    user to be able to set flight information in any way. The information
    for the flights will be initialized in the constructor

--- src/main/java/model/FlightInformation.java
+++ src/main/java/model/FlightInformation.java
@@ -20,26 +20,15 @@ public class FlightInformation {
 	public int getFlightNumber() {
 		return flightNumber;
 	}
-	public void setFlightNumber(int flightNumber) {
-		this.flightNumber = flightNumber;
-	}
 	public String getDepartureLocation() {
 		return departureLocation;
-	}
-	public void setDepartureLocation(String departureLocation) {
-		this.departureLocation = departureLocation;
 	}
 	public String getArrivalLocation() {
 		return arrivalLocation;
 	}
-	public void setArrivalLocation(String arrivalLocation) {
-		this.arrivalLocation = arrivalLocation;
-	}
 	public Date getDate() {
 		return date;
 	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
+
 
 }

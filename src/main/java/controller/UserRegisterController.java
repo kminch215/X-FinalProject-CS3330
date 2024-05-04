259776582f64b4cfd57801bb0608e10db6618e44
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-04 18:39:11 -0500

    Switched UserRegisterController to JFrame

--- src/main/java/controller/UserRegisterController.java
+++ src/main/java/controller/UserRegisterController.java
@@ -1,50 +1,51 @@
 package controller;
 
+import model.UserInformation;
 import model.UserListModel;
 import view.UserRegisterView;
 
+import javax.swing.*;
+
 public class UserRegisterController {
+    private UserRegisterView registerView;
+    private UserListModel userModel;
+    private static UserRegisterController instance;
 
-	private UserRegisterView registerView;
-	private UserListModel userModel;
-	private static UserRegisterController instance;
-	
-	public UserRegisterController() {
-		super();
-		this.registerView = new UserRegisterView();
-		this.userModel = new UserListModel();
-	}
+    public UserRegisterController() {
+        this.registerView = new UserRegisterView();
+        this.userModel = new UserListModel();
+    }
 
-	public static UserRegisterController getInstance() {
+    public static UserRegisterController getInstance() {
         if (instance == null) {
             instance = new UserRegisterController();
         }
         return instance;
     }
 
-	public void registerUser(String username, String password, String firstName, String lastName, String email) {
+    public void registerUser(String username, String password, String firstName, String lastName, String email) {
         if (validateInput(username, password, firstName, lastName, email)) {
             // Generate a unique user ID
             int userID = generateUserID();
-            
+
             // Create a new user
             UserInformation newUser = new UserInformation(username, password, firstName, lastName, email, userID);
-            
+
             // Save the user
             userModel.saveUser(newUser);
-            
+
             // Notify user of a successful registration
-            registerView.displaySuccessMessage("User registered successfully!");
+            JOptionPane.showMessageDialog(null, "User registered successfully!");
         } else {
-            registerView.displayErrorMessage("Invalid input. Please fill all fields.");
+            JOptionPane.showMessageDialog(null, "Invalid input. Please fill all fields.", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
-    
+
     private boolean validateInput(String username, String password, String firstName, String lastName, String email) {
         return !username.isEmpty() && !password.isEmpty() && !firstName.isEmpty() && !lastName.isEmpty() && !email.isEmpty();
     }
-    
+
     private int generateUserID() {
-        return (int) (Math.random() * 1000); 
+        return (int) (Math.random() * 1000);
     }
 }

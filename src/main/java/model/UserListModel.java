2fca04f3caba178e1bc1160c028d0a2c06b506d0
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-04 18:36:46 -0500

    Switched UserListModel to JFrame

--- src/main/java/model/UserListModel.java
+++ src/main/java/model/UserListModel.java
@@ -1,45 +1,43 @@
 package model;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class UserListModel {
+    private ArrayList<UserInformation> userList;
 
-	private ArrayList<UserInformation> userList;
-
-	public UserListModel() {
-		super();
-		this.userList = new ArrayList<UserInformation>();
-	}
-
-	public ArrayList<UserInformation> getUserList() {
-		return userList;
-	}
-
-	public void setUserList(ArrayList<UserInformation> userList) {
-		this.userList = userList;
-	}
-
-	public void saveUser(UserInformation user) {
-        	userList.add(user);
-        	System.out.println("User registered successfully!");
+    public UserListModel() {
+        this.userList = new ArrayList<>();
     }
 
-	public UserInformation getUserByUsername(String username) {
+    public ArrayList<UserInformation> getUserList() {
+        return userList;
+    }
+
+    public void setUserList(ArrayList<UserInformation> userList) {
+        this.userList = userList;
+    }
+
+    public void saveUser(UserInformation user) {
+        userList.add(user);
+        JOptionPane.showMessageDialog(null, "User registered successfully!");
+    }
+
+    public UserInformation getUserByUsername(String username) {
         for (UserInformation user : userList) {
             if (user.getUsername().equals(username)) {
                 return user;
             }
         }
-        return null; 
+        return null;
     }
-    
-    	public UserInformation authenticateUser(String username, String password) {
-        	for (UserInformation user : userList) {
-            	if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
-                	return user; // If Successful
-            	}
+
+    public UserInformation authenticateUser(String username, String password) {
+        for (UserInformation user : userList) {
+            if (user.getUsername().equals(username) && user.getPassword().equals(password)) {
+                return user; // If Successful
+            }
         }
         return null; // If Fail
     }
-	
 }

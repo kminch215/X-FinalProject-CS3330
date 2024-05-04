745008e0806360c895658e1ccbe2d17fb2a29413
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-04 18:37:34 -0500

    Switched UserInformation to JFrame

--- src/main/java/model/UserInformation.java
+++ src/main/java/model/UserInformation.java
@@ -1,62 +1,69 @@
 package model;
 
-public class UserInformation {
+import javax.swing.*;
 
-	private int userID;
-	private String username;
-	private String password;
-	private String firstName;
-	private String lastName;
-	private String email;
-	
-	
-	public UserInformation(int userID, String username, String password, String firstName, String lastName,
-			String email) {
-		super();
-		this.userID = userID;
-		this.username = username;
-		this.password = password;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-	}
-	
-	public int getUserID() {
-		return userID;
-	}
-	public void setUserID(int userID) {
-		this.userID = userID;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	 
+public class UserInformation {
+    private int userID;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public UserInformation(int userID, String username, String password, String firstName, String lastName, String email) {
+        this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }

38a9d3a21c5e3fa9b6ab5505ca7065d82ecf1add
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-04 19:12:03 -0500

    Switched UserDashboardView to JFrame

--- src/main/java/view/UserDashboardView.java
+++ src/main/java/view/UserDashboardView.java
@@ -1,15 +1,52 @@
 package view;
 
-public class UserDashboardView {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDashboardView extends JFrame {
+    private JLabel userIDLabel;
+    private JLabel currentTicketsLabel;
+    private JLabel ticketHistoryLabel;
+    
+    public UserDashboardView() {
+        setTitle("User Dashboard");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 1));
+        
+        userIDLabel = new JLabel();
+        add(userIDLabel);
+        
+        currentTicketsLabel = new JLabel();
+        add(currentTicketsLabel);
+        
+        ticketHistoryLabel = new JLabel();
+        add(ticketHistoryLabel);
+        
+        setVisible(true);
+    }
+    
     public void displayUserProfile(int userID) {
-        System.out.println("User Profile for UserID: " + userID);
+        userIDLabel.setText("User Profile for UserID: " + userID);
     }
     
-    public void displayCurrentTickets() {
-        System.out.println("Displaying Current Tickets...");
+    public void displayCurrentTickets(List<String> tickets) {
+        StringBuilder ticketsText = new StringBuilder("<html>Current Tickets:<br>");
+        for (String ticket : tickets) {
+            ticketsText.append(ticket).append("<br>");
+        }
+        ticketsText.append("</html>");
+        currentTicketsLabel.setText(ticketsText.toString());
     }
     
-    public void displayTicketHistory() {
-        System.out.println("Displaying Ticket History...");
+    public void displayTicketHistory(List<String> history) {
+        StringBuilder historyText = new StringBuilder("<html>Ticket History:<br>");
+        for (String event : history) {
+            historyText.append(event).append("<br>");
+        }
+        historyText.append("</html>");
+        ticketHistoryLabel.setText(historyText.toString());
     }
 }

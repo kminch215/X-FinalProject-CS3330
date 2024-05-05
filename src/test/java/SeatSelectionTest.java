6d19d491bcb2b74529af3533fb89c56cd0d6f20b
Samantha Whitaker <s.whita6@gmail.com>
2024-05-04 20:13:40 -0500

    Added additional comments to SeatListModel class  and cleaned up code

--- src/test/java/SeatSelectionTest.java
+++ src/test/java/SeatSelectionTest.java
@@ -3,13 +3,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import javax.swing.DefaultListModel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 

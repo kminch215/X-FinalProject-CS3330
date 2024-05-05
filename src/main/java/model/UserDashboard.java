7a8434603bf6bdcd338e70593821fd2d8fcf5618
LPCardinals <116538923+LPCardinals@users.noreply.github.com>
2024-05-04 19:10:07 -0500

    Switched UserDashboard to JFrame

--- src/main/java/model/UserDashboard.java
+++ src/main/java/model/UserDashboard.java
@@ -1,27 +1,25 @@
 package model;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class UserDashboard {
+    private UserInformation user;
+    private ArrayList<ReceiptInformation> purchaseHistory;
 
-	private UserInformation user;
-	private ArrayList<ReceiptInformation> purchaseHistory;
-	
-	public UserDashboard(UserInformation user) {
-		super();
-		this.user = user;
-		purchaseHistory = new ArrayList<>();
-		
-	}
-	
-	public int getUserID() {
-        return userID;
+    public UserDashboard(UserInformation user) {
+        this.user = user;
+        purchaseHistory = new ArrayList<>();
     }
-    
+
+    public int getUserID() {
+        return user.getUserID();
+    }
+
     public void addPurchase(ReceiptInformation receipt) {
         purchaseHistory.add(receipt);
     }
-    
+
     public ArrayList<ReceiptInformation> getPurchaseHistory() {
         return purchaseHistory;
     }

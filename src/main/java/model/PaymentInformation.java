fb55ee0da11b4a4a1710d8a777c23d8b01531113
Sarah Greenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-05-05 22:44:48 -0500

    Update PaymentInformation.java
    Added methods for storage purposes

--- src/main/java/model/PaymentInformation.java
+++ src/main/java/model/PaymentInformation.java
@@ -16,11 +16,43 @@ public class PaymentInformation {
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
-	public Long getCreditCardNo() {
+public Long getCreditCardNo() {
 		return creditCardNo;
 	}
 	public void setCreditCardNo(Long creditCardNo) {
 		this.creditCardNo = creditCardNo;
-	}	
+	}
+	public String getCardNumber() {
+        return cardNumber;
+    }
+
+//specific methods storing for card information
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCvvNumber() {
+        return cvvNumber;
+    }
+
+    public void setCvvNumber(String cvvNumber) {
+        this.cvvNumber = cvvNumber;
+    }
+
+    public String getExpirationMonth() {
+        return expirationMonth;
+    }
+
+    public void setExpirationMonth(String expirationMonth) {
+        this.expirationMonth = expirationMonth;
+    }
+
+    public String getExpirationYear() {
+        return expirationYear;
+    }
+
+    public void setExpirationYear(String expirationYear) {
+        this.expirationYear = expirationYear;
+    }
 	
 }
